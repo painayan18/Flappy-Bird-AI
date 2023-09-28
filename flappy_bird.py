e3faef6470c55cdbfa80291cc61dd757d258d1bc
nayan <nayanspai@gmail.com>
2023-09-27 17:16:32 -0700

    refactor images > imgs

--- flappy_bird.py
+++ flappy_bird.py
@@ -8,7 +8,7 @@ import random
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
 
-# import images from /imgs
+# import imgs from /imgs
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
              pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
              pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
